60919d3771555ed008f98e1e4b93b06e115524c7
Douglas <Douglas@Douglas>
2022-05-04 19:05:32 -0300

    MODULO 16 AULA 9

---
+++ Arquivos/src/EscreveJSON2.java
@@ -0,0 +1,20 @@
+
+public class EscreveJSON2 {
+	
+	public static void main(String[] args) {
+		
+		Usuario usuario1 = new Usuario();
+		usuario1.setCpf("25652456");
+		usuario1.setNome("Douglas");
+		usuario1.setSenha("123456");
+		usuario1.setLogin("doug");
+		
+		Usuario usuario2 = new Usuario();
+		usuario2.setCpf("1111111");
+		usuario2.setNome("Userrr");
+		usuario2.setSenha("123456");
+		usuario2.setLogin("user");
+		
+	}
+
+}
